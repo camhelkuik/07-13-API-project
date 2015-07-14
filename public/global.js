2242820f99806713bd4f2b299f34b87af134817a
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-14 14:25:45 -0500

    Have JS working, currently returning array of object

--- public/global.js
+++ public/global.js
@@ -1,34 +1,36 @@
-var allAssign = function(){
-var req = new XMLHttpRequest();
+var showAll = function(event){
+  var req = new XMLHttpRequest();
+  var href = this.getAttribute("href");
 
-req.open("get", "http://localhost:4567/api/assignments");
+  req.open("get", href);
 
-req.addEventListener("load", function(event){
-  var container = document.getElementById = ("main");
-  container.innerHTML= this.response;
+  req.addEventListener("load", function(){
+    var container = document.getElementById("main");
+    container.innerHTML= this.response;
+  })
   event.preventDefault();
-})
+  req.responseType = "json";
+  req.send();
 }
 
-req.responseType = "json";
-req.send();
+
+// var singleAssign = function(){
+// var req = new XMLHttpRequest();
+//
+// req.open("get", "/api/assignment_id");
+//
+// req.addEventListener("load", function(){
+//   for (var i = 0; i < this.response.length; i++){
+//     var container = document.getElementById = ("single");
+//     container.innerHTML = this.response[i];
+//   }
+// })
+// }
+//
+// req.responseType = "json";
+// req.send();
 
 
-var singleAssign = function(){
-var req = new XMLHttpRequest();
-
-req.open("get", "/api_assign_list/:id");
-
-req.addEventListener("load", function(){
-  for (var i = 0; i < this.response.length; i++){
-    var container = document.getElementById = ("single");
-    container.innerHTML = this.response[i];
-  }
-})
-}
-
-req.responseType = "json";
-req.send();
-
-
-document.getElementById("all_assign").onclick = allAssign;
+document.getElementById("all_assign").onclick = showAll;
+document.getElementById("all_links").onclick = showAll;
+document.getElementById("all_partners").onclick = showAll;
